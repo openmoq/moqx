b4d7329aba68392ce6be7dbd7f310db6ff14df6a
Michal Hosna <mh@michalhosna.com>
2026-03-15 23:00:34 +0000

    Wire multi-service routing into ORelayServer

--- include/o_rly/ORelayServer.h
+++ include/o_rly/ORelayServer.h
@@ -2,6 +2,8 @@
 
 #include <moxygen/MoQServer.h>
 #include <o_rly/ORelay.h>
+#include <o_rly/ServiceMatcher.h>
+#include <o_rly/config/config.h>
 
 namespace openmoq::o_rly {
 
@@ -13,19 +15,23 @@ public:
       const std::string& key,
       const std::string& endpoint,
       const std::string& versions,
-      size_t maxCachedTracks,
-      size_t maxCachedGroupsPerTrack
+      std::vector<config::ServiceConfig> services
   );
 
   // Used when the insecure flag is true
   ORelayServer(
       const std::string& endpoint,
       const std::string& versions,
-      size_t maxCachedTracks,
-      size_t maxCachedGroupsPerTrack
+      std::vector<config::ServiceConfig> services
   );
 
   void onNewSession(std::shared_ptr<moxygen::MoQSession> clientSession) override;
+
+  folly::Expected<folly::Unit, moxygen::SessionCloseErrorCode> validateAuthority(
+      const moxygen::ClientSetup& clientSetup,
+      uint64_t negotiatedVersion,
+      std::shared_ptr<moxygen::MoQSession> session
+  ) override;
 
 protected:
   std::shared_ptr<moxygen::MoQSession> createSession(
@@ -34,7 +40,10 @@ protected:
   ) override;
 
 private:
-  std::shared_ptr<ORelay> relay_;
+  void initRelays(const std::vector<config::ServiceConfig>& services);
+
+  std::vector<std::shared_ptr<ORelay>> relays_;
+  ServiceMatcher serviceMatcher_;
 };
 
 } // namespace openmoq::o_rly
