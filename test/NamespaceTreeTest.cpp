1d821e8069ae26e276de362198a3a2a09eeb531b
Michal Hosna <mh@michalhosna.com>
2026-07-20 16:55:14 +0000

    submodules: remove moxygen and catapult; repoint license headers
    - Dead weight once CPM supplies both.
    - Split from the CPM wiring so the license-header churn stays a separate,
      mechanical diff.

--- test/NamespaceTreeTest.cpp
+++ test/NamespaceTreeTest.cpp
@@ -1,7 +1,8 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * Originally from github.com/facebookexperimental/moxygen.
- * See deps/moxygen/LICENSE file in the root directory for license terms.
+ * See the moxygen LICENSE (https://github.com/openmoq/moxygen) file in the root directory for
+ * license terms.
  *
  * Copyright (c) OpenMOQ contributors.
  */
