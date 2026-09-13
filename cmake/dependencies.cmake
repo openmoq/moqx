d34d51bffff2bfaf85add3943bd23a1af4905b1d
afrind <8259689+afrind@users.noreply.github.com>
2026-09-13 18:25:04 -0400

    Bump moxygen pin to pico-enchilada

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -15,7 +15,7 @@
 # on the next reconfigure of an existing build dir. A cached pin would silently
 # shadow the file's value.
 set(MOXYGEN_REPOSITORY "openmoq/moxygen")
-set(MOXYGEN_REV "93077972785ccb13c0c4d0731729c63d09bbb269")
+set(MOXYGEN_REV "e38a0f7b0eb5c118e6a17aed5e771431284e7bac")
 
 set(CATAPULT_REPOSITORY "Quicr/catapult")
 set(CATAPULT_REV "2bbf479fe2e65e425624316d335443a8c0fc0507")
